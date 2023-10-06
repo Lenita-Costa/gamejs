fed9c40f65f5c9d62926b3a937bbde3876b66e87
Lenita Costa <lenita@fenasbac.com.br>
2023-10-06 16:08:14 -0300

    Criando função de reiniciar o jogo

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ function exibirTextoNaTela(tag, texto) {
   let campo = document.querySelector(tag);
   campo.innerHTML = texto;
 }
-
-exibirTextoNaTela("h1", "Jogo do número secreto");
-exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
+function exibirMensagemInicial() {
+  exibirTextoNaTela("h1", "Jogo do número secreto");
+  exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
+}
+exibirMensagemInicial();
 
 function verificarChute() {
   let chute = document.querySelector("input").value;
@@ -38,4 +40,12 @@ function gerarNumeroAleatorio() {
 function limparCampo() {
   chute = document.querySelector("input");
   chute.value = " ";
+}
+
+function reiniciarJogo() {
+  numeroSecreto = gerarNumeroAleatorio();
+  limparCampo();
+  tentativas = 1;
+  exibirMensagemInicial();
+  document.getElementById("reiniciar").setAttribute("disabled", true);
 }
