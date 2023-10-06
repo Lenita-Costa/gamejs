766f0fc48c22500d69574b289d075204c7262ef0
Lenita Costa <lenita@fenasbac.com.br>
2023-10-06 11:34:33 -0300

    Criando funções com retorno

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-function exibirTextoNaTela(tag) {
+function exibirTextoNaTela(tag, texto) {
   let campo = document.querySelector(tag);
   campo.innerHTML = texto;
 }
 
-exibirTextoNaTela("h1");
-exibirTextoNaTela("p");
+exibirTextoNaTela("h1", "Jogo do número secreto");
+exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
+
+function verificarChute() {
+  console.log("O botão foi clicado!");
+}
+
+function gerarNumeroAleatorio() {
+  parseFloat(Math.random() * 10 + 1);
+}
